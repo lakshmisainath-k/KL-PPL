f04d9292384dd5a5e90aac286fa66fb2e3088830
lakshmisainath-k <lakshmisainathkonisetty@gmail.com>
2026-02-22 09:58:34 +0530

    Add files via upload

---
+++ service-worker 2.js
@@ -0,0 +1,34 @@
+const CACHE = "ppl-tracker-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./data.js",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then(cache => cache.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(() => cached))
+  );
+});
